3f48a863a7b11fc186ee2579dea6b871e0907ca7
JhonDenard <firme.jhondenard@gmail.com>
2025-08-06 10:51:53 +0800

    Fixed Async Bug

--- app/api/questions/[id]/route.ts
+++ app/api/questions/[id]/route.ts
@@ -1,20 +1,18 @@
 import { MegaQuestions } from "@/data/questions";
 import { NextResponse } from "next/server";
 
-export async function GET ( // GET has 2 Parameters. Request Object and Params Context (Shortcut in NextJS)
-    request: Request, 
-    {params}: {params: {id:string}}) : Promise<NextResponse> {  //NextResponse is a Promise. In regular cases, just NextResponse
-    
-    const {id} = await params;  // Await the params first
-    const q_id = Number(id);    // Then convert to a Number
+export function GET( // GET has 2 Parameters. Request Object and Params Context (Shortcut in NextJS)
+    request: Request,
+    { params }: { params: { id: string } }): NextResponse {  //NextResponse is a Promise. In regular cases, just NextResponse
 
-    // Find the matching ID. Find in questions (q) where question.id === to the Q_ID We converted from the URL Param
-    const question = MegaQuestions.find((q) => q.id === q_id);
+    const id = Number (params.id)  // Convert to Num
+    // Find the matching ID. Find in questions (q) where question.id === to the id We converted from the URL Param
+    const question = MegaQuestions.find((q) => q.id === id);
 
-    if (!question){
+    if (!question) {
         return NextResponse.json(
-            {error: `QuestionID = ${id} not found`},
-            {status: 404}
+            { error: `QuestionID = ${id} not found` },
+            { status: 404 }
         )
     }
 
